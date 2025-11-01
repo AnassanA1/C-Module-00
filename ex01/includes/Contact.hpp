ff3690674535ffad9028110919d38924ab033651
AnassanA1 <teamatlasdesign@gmail.com>
2025-11-01 18:20:34 +0100

    last push?

--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -3,17 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azghibat <azghibat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anasszgh <anasszgh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 17:56:14 by azghibat          #+#    #+#             */
-/*   Updated: 2025/10/18 12:43:23 by azghibat         ###   ########.fr       */
+/*   Updated: 2025/10/31 16:16:05 by anasszgh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
-
-
 
 #include <string>
 
@@ -23,21 +21,22 @@ private:
     std::string lastname;
     std::string phone;
     std::string nickname;
+    std::string darkestSecret;
 
 public:
-    Contact(); // default constructor
+    Contact();
 
-    // Setters
     void setfirstname(const std::string &fn);
     void setlastname(const std::string &ln);
     void setphone(const std::string &pn);
     void setnickname(const std::string &nn);
+    void setDarkestSecret(const std::string &ds);
 
-    // Getters
     std::string getfirstname() const;
     std::string getlastname() const;
     std::string getphone() const;
     std::string getnickname() const;
+    std::string getDarkestSecret() const;
 };
 
 #endif
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azghibat <azghibat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anasszgh <anasszgh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:03:03 by azghibat          #+#    #+#             */
-/*   Updated: 2025/10/23 11:01:20 by azghibat         ###   ########.fr       */
+/*   Updated: 2025/10/31 16:24:25 by anasszgh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <cstdlib>  // For atoi (C++98 compliant)
 
 bool isNumber(const std::string &str)
 {
@@ -35,12 +36,16 @@ int main()
     while (true)
     {
         std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        std::getline(std::cin, command);
+        
+        if (!std::getline(std::cin, command))
+        {
+            std::cout << std::endl;
+            std::cout << "getline failure" << std::endl;
+            exit(0);
+        }
 
         if (command == "ADD")
-        {
-            phonebook.addContact();
-        }
+            phonebook.addContact();   
         else if (command == "SEARCH")
         {
             if (phonebook.getCount() == 0)
@@ -48,28 +53,24 @@ int main()
                 std::cout << "PhoneBook is empty!" << std::endl;
                 continue;
             }
-
+            
             phonebook.displayContacts();
-
             std::cout << "Enter index to view: ";
             std::string idx_input;
             std::getline(std::cin, idx_input);
-
+            
             if (!isNumber(idx_input))
             {
                 std::cout << "invalid input!" << std::endl;
                 continue;
             }
-
-            int idx = std::stoi(idx_input);
+            
+            int idx = std::atoi(idx_input.c_str());
+            
             if (idx < 0 || idx >= phonebook.getCount() || idx >= 8)
-            {
                 std::cout << "invalid index!" << std::endl;
-            }
             else
-            {
                 phonebook.displayContact(idx);
-            }
         }
         else if (command == "EXIT")
         {
@@ -77,9 +78,7 @@ int main()
             break;
         }
         else
-        {
             std::cout << "input invalid or unknow command" << std::endl;
-        }
     }
 
     return 0;
